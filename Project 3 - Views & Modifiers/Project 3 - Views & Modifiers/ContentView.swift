e59e0a2a6a3d297b9431e7fd26a46da1589310d0
sanika chavan <sanikac10@gmail.com>
2022-01-17 21:02:45 +0530

    New Update

---
+++ Project 3 - Views & Modifiers/Project 3 - Views & Modifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 3 - Views & Modifiers
+//
+//  Created by sanika chavan on 17/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 3 - Views & Modifiers/Project 3 - Views & Modifiers/Project_3___Views___ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_3___Views___ModifiersApp.swift
+//  Project 3 - Views & Modifiers
+//
+//  Created by sanika chavan on 17/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Project_3___Views___ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
