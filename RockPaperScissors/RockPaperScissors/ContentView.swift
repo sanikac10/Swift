dc9a2ad7a301e526605939d17c1afa3292308db0
sanika chavan <sanikac10@gmail.com>
2022-02-12 23:51:18 +0530

    Base

---
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by sanika chavan on 12/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var currentChoice: () = ()
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by sanika chavan on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
