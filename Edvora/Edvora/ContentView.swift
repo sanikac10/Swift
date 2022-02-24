112156d4d1a019e84f67d69985a28ecdc6bc2480
sanika chavan <sanikac10@gmail.com>
2022-02-24 18:19:11 +0530

    Base 2

--- Edvora/Edvora/ContentView.swift
+++ Edvora/Edvora/ContentView.swift
@@ -10,20 +10,64 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-            
             Text("Edvora")
-                .font(.largeTitle.weight(.bold))
-                .foregroundColor(.black)
-        VStack(spacing: 15){
-            Text("Map")
-            
+                .bold()
+                .font(.largeTitle)
+            Spacer()
+            HStack(spacing: 25){
+            Text("Nearest")
+                    .bold()
+            Text("Upcoming(4)")
+                    .bold()
+            Text("Past(2)")
+                    .bold()
+                Spacer()
+            Text("Filters")
+            }
+        ScrollView(.vertical, showsIndicators: false){
+            VStack(spacing: 15){
+                HStack{
+                    Text("#")
+                        .foregroundColor(.blue)
+                    Text("Ride ID")
+                }.frame(maxWidth: 400)
+                    .padding(.vertical, 20)
+                    .background(.thinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+
+                HStack{
+                    Text("#")
+                        .foregroundColor(.blue)
+                    Text("Ride ID")
+                }.frame(maxWidth: 400)
+                    .padding(.vertical, 20)
+                    .background(.thinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+
+                HStack{
+                    Text("#")
+                        .foregroundColor(.blue)
+                    Text("Ride ID")
+                }.frame(maxWidth: 400)
+                    .padding(.vertical, 20)
+                    .background(.thinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+
+                HStack{
+                    Text("#")
+                        .foregroundColor(.blue)
+                    Text("Ride ID")
+                }.frame(maxWidth: 400)
+                    .padding(.vertical, 20)
+                    .background(.thinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+
+
+            }        }
         }
-        .frame(maxWidth: .infinity)
-        .padding(.vertical, 20)
-        .background(.thinMaterial)
-        .clipShape(RoundedRectangle(cornerRadius: 50))
+
     }
-}
+    
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
