6a8d32281befc783d6fb5ea9a24bcd90d40f649c
sanika chavan <sanikac10@gmail.com>
2022-02-25 14:18:58 +0530

    Submission Edvora

--- Edvora/Edvora/ContentView.swift
+++ Edvora/Edvora/ContentView.swift
@@ -8,69 +8,159 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 0
     var body: some View {
         VStack{
+            HStack{
             Text("Edvora")
                 .bold()
                 .font(.largeTitle)
+                .padding()
+                Spacer()
+            Image("Memoji2")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .padding()
+            }
+            .padding(.horizontal, 10)
             Spacer()
             HStack(spacing: 25){
             Text("Nearest")
                     .bold()
+                    .underline( color: .blue)
             Text("Upcoming(4)")
-                    .bold()
+                    .foregroundColor(.gray)
             Text("Past(2)")
-                    .bold()
+                    .foregroundColor(.gray)
                 Spacer()
+// filter
+                Menu(){
+                    
+                    Button(action: {}, label: {
+                        Text("City")
+                    })
+                    
+                    Picker(selection: $selection, label: Text("City")){
+                        Text("Mumbai")
+                        Text("Pune")
+                        Text("Delhi")
+                        Text("Bangalore")
+                    }
+                } label: {
+                HStack{
+                    Image("Filter")
+                        .resizable()
+                        .frame(width: 15, height: 15)
             Text("Filters")
+                        .foregroundColor(.black)
+                }
+
             }
+            }
+//ScrollView Start
         ScrollView(.vertical, showsIndicators: false){
-            VStack(spacing: 15){
-                HStack{
+            VStack(spacing: 10){
+                VStack{
+                        Image("PastedGraphic")
+                            .resizable()
+                    HStack{
+                    HStack{
                     Text("#")
                         .foregroundColor(.blue)
                     Text("Ride ID")
-                }.frame(maxWidth: 400)
-                    .padding(.vertical, 20)
+                    }
+                        Spacer()
+                        HStack{
+                        Image("Calender")
+                                .resizable()
+                                .frame(width:20, height: 20)
+                        Text("Date")
+                        }
+                    }
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding(30)
                     .background(.thinMaterial)
                     .clipShape(RoundedRectangle(cornerRadius: 15))
-
+//
+                VStack{
+                    Image("PastedGraphic")
+                        .resizable()
                 HStack{
-                    Text("#")
-                        .foregroundColor(.blue)
-                    Text("Ride ID")
-                }.frame(maxWidth: 400)
-                    .padding(.vertical, 20)
-                    .background(.thinMaterial)
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-
                 HStack{
-                    Text("#")
-                        .foregroundColor(.blue)
-                    Text("Ride ID")
-                }.frame(maxWidth: 400)
-                    .padding(.vertical, 20)
-                    .background(.thinMaterial)
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-
+                Text("#")
+                    .foregroundColor(.blue)
+                Text("Ride ID")
+                }
+                    Spacer()
+                    HStack{
+                    Image("Calender")
+                            .resizable()
+                            .frame(width:20, height: 20)
+                    Text("Date")
+                    }
+                }
+                    
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(30)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+//
+                VStack{
+                    Image("PastedGraphic")
+                        .resizable()
                 HStack{
-                    Text("#")
-                        .foregroundColor(.blue)
-                    Text("Ride ID")
-                }.frame(maxWidth: 400)
-                    .padding(.vertical, 20)
+                HStack{
+                Text("#")
+                    .foregroundColor(.blue)
+                Text("Ride ID")
+                }
+                    Spacer()
+                    HStack{
+                    Image("Calender")
+                            .resizable()
+                            .frame(width:20, height: 20)
+                    Text("Date")
+                    }
+                }
+                    
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(30)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+//
+                VStack{
+                    Image("PastedGraphic")
+                        .resizable()
+                HStack{
+                HStack{
+                Text("#")
+                    .foregroundColor(.blue)
+                Text("Ride ID")
+                }
+                    Spacer()
+                    HStack{
+                    Image("Calender")
+                            .resizable()
+                            .frame(width:20, height: 20)
+                    Text("Date")
+                    }
+                }
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding(30)
                     .background(.thinMaterial)
                     .clipShape(RoundedRectangle(cornerRadius: 15))
-
-
-            }        }
+            
+            
         }
-
-    }
+        }
+            
+        }
     
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
